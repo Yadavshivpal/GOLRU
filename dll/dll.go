9b07dbc61eddd54e4e3048b4ad5baf21b51ec2e0
Shivpal Yadav <shivayadav456.sy@gmail.com>
2021-06-14 21:18:39 +0530

    Create dll.go

---
+++ dll/dll.go
@@ -0,0 +1,45 @@
+package dll
+
+type Node struct {
+    Key int
+    Next *Node
+    Prev *Node
+}
+
+type List struct {
+    Head *Node
+    Tail *Node
+}
+
+type Entry struct {
+    Value int
+    N *Node
+}
+
+func Movenode(li List, n *Node) {
+    Head := li.Head
+
+    n.Next.Prev = n.Prev
+    n.Prev.Next = n.Next
+
+    n.Next = Head.Next
+    Head.Next.Prev = n
+
+    n.Prev = Head
+    Head.Next = n
+}
+
+func Insertnode(li List, n *Node) {
+    Head := li.Head
+
+    Head.Next.Prev = n
+    n.Next = Head.Next
+    n.Prev = Head
+    Head.Next = n
+}
+
+func Removenode(li List) {
+    Tail := li.Tail
+    Tail.Prev.Prev.Next = Tail
+    Tail.Prev = Tail.Prev.Prev
+}
