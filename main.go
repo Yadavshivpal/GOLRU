0384bf005b868dddee4c73d367b1a0bf4577cdd2
Shivpal Yadav <shivayadav456.sy@gmail.com>
2021-06-14 22:21:45 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import l "github.com/Yadavshivpal/GOLRU/lru"
+
+func main(){
+
+  obj := l.Constructor(2)
+  obj.Put(2,20)
+  obj.Put(3,30)
+  obj.Put(1,10)
+  out := obj.Get(3)
+  fmt.Println(out)
+  out1 := obj.Get(1)
+  fmt.Println(out1)
+
+}
